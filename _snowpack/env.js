94eb26ed1e3d398bff4c880a67552e57b999f518
johnb8005 <johnb8005@users.noreply.github.com>
2021-05-22 06:35:44 +0000

    deploy: 974b79ac67b14bd59f35d1a286302526482edcd7

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "ee37f7480e5958b5dc45e5822b2295ab92a6d5c3";
+export const SNOWPACK_PUBLIC_GIT_SHA = "974b79ac67b14bd59f35d1a286302526482edcd7";
 export const SNOWPACK_PUBLIC_URL = "/boilerplate_snowpack_bootstrap";
 export const MODE = "development";
 export const NODE_ENV = "development";
