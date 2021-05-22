3d8220f7e2b2b28c2a91de23e90eb0a24c8408b8
Johan <johan@macair2020>
2021-05-22 08:19:16 +0200

    change GH url

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export const sha = import.meta.env.SNOWPACK_PUBLIC_GIT_SHA || "arandomsha";
 export const title = import.meta.env.SNOWPACK_PUBLIC_TITLE || "Boilerplate";
 export const basename = import.meta.env.SNOWPACK_PUBLIC_URL;
 
-const ghUrl = "https://github.com/nexys-admin/payroll-switzerland";
+const ghUrl = "https://github.com/nexys-system/boilerplate_snowpack_bootstrap";
 
 export const github = { sha: `${ghUrl}/commit/${sha}`, url: ghUrl };
