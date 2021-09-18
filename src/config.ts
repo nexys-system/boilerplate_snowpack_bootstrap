bf7081074e68ac3c0af78499b8ed94d054d5fd9e
John B <johan@nexys.ch>
2021-09-18 13:28:51 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const sha = import.meta.env.SNOWPACK_PUBLIC_VERSION || "unset_version";
+export const version = import.meta.env.SNOWPACK_PUBLIC_VERSION || "unset_version";
 export const sha = import.meta.env.SNOWPACK_PUBLIC_GIT_SHA || "unset_sha";
 export const title = import.meta.env.SNOWPACK_PUBLIC_TITLE || "Boilerplate";
 export const basename = import.meta.env.SNOWPACK_PUBLIC_URL;
