623dca110831115d05d103643c0fc7f91ede5b2f
johnb8005 <johnb8005@users.noreply.github.com>
2021-09-18 11:29:48 +0000

    deploy: bf7081074e68ac3c0af78499b8ed94d054d5fd9e

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,5 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "3fd1b0f819fe793beeb61f72d0b83e1724599bb6";
+export const SNOWPACK_PUBLIC_GIT_SHA = "bf7081074e68ac3c0af78499b8ed94d054d5fd9e";
+export const SNOWPACK_PUBLIC_VERSION = "v0.0.2";
 export const SNOWPACK_PUBLIC_URL = "/boilerplate_snowpack_bootstrap";
 export const MODE = "development";
 export const NODE_ENV = "development";
--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 import * as __SNOWPACK_ENV__ from './_snowpack/env.js';
 
-export const sha = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_GIT_SHA || "arandomsha";
+export const version = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_VERSION || "unset_version";
+export const sha = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_GIT_SHA || "unset_sha";
 export const title = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_TITLE || "Boilerplate";
 export const basename = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_URL;
 const ghUrl = "https://github.com/nexys-system/boilerplate_snowpack_bootstrap";
