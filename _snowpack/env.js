2888a6533fde196d0249f50966e23cf4c1f598e8
johnb8005 <johnb8005@users.noreply.github.com>
2022-02-23 23:24:52 +0000

    deploy: da4a07b385bbf89073a6276de4fb7d04a96c298a

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,6 +1,6 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "bf7081074e68ac3c0af78499b8ed94d054d5fd9e";
-export const SNOWPACK_PUBLIC_VERSION = "v0.0.2";
+export const SNOWPACK_PUBLIC_GIT_SHA = "da4a07b385bbf89073a6276de4fb7d04a96c298a";
+export const SNOWPACK_PUBLIC_VERSION = "v0.0.3";
 export const SNOWPACK_PUBLIC_URL = "/boilerplate_snowpack_bootstrap";
-export const MODE = "development";
-export const NODE_ENV = "development";
+export const MODE = "production";
+export const NODE_ENV = "production";
 export const SSR = false;
--- _snowpack/pkg/react-router-dom.js
+++ _snowpack/pkg/react-router-dom.js
@@ -195,7 +195,7 @@ function invariant(condition, message) {
     }
 }
 
-function addLeadingSlash$1(path) {
+function addLeadingSlash(path) {
   return path.charAt(0) === '/' ? path : '/' + path;
 }
 function stripLeadingSlash(path) {
@@ -204,7 +204,7 @@ function stripLeadingSlash(path) {
 function hasBasename(path, prefix) {
   return path.toLowerCase().indexOf(prefix.toLowerCase()) === 0 && '/?#'.indexOf(path.charAt(prefix.length)) !== -1;
 }
-function stripBasename$1(path, prefix) {
+function stripBasename(path, prefix) {
   return hasBasename(path, prefix) ? path.substr(prefix.length) : path;
 }
 function stripTrailingSlash(path) {
@@ -433,7 +433,7 @@ function createBrowserHistory(props) {
     props = {};
   }
 
-  !canUseDOM ? invariant(false) : void 0;
+  !canUseDOM ?  invariant(false) : void 0;
   var globalHistory = window.history;
   var canUseHistory = supportsHistory();
   var needsHashChangeListener = !supportsPopStateOnHashChange();
@@ -444,7 +444,7 @@ function createBrowserHistory(props) {
       getUserConfirmation = _props$getUserConfirm === void 0 ? getConfirmation : _props$getUserConfirm,
       _props$keyLength = _props.keyLength,
       keyLength = _props$keyLength === void 0 ? 6 : _props$keyLength;
-  var basename = props.basename ? stripTrailingSlash(addLeadingSlash$1(props.basename)) : '';
+  var basename = props.basename ? stripTrailingSlash(addLeadingSlash(props.basename)) : '';
 
   function getDOMLocation(historyState) {
     var _ref = historyState || {},
@@ -456,7 +456,7 @@ function createBrowserHistory(props) {
         search = _window$location.search,
         hash = _window$location.hash;
     var path = pathname + search + hash;
-    if (basename) path = stripBasename$1(path, basename);
+    if (basename) path = stripBasename(path, basename);
     return createLocation(path, state, key);
   }
 
@@ -679,11 +679,11 @@ var HashPathCoders = {
   },
   noslash: {
     encodePath: stripLeadingSlash,
-    decodePath: addLeadingSlash$1
+    decodePath: addLeadingSlash
   },
   slash: {
-    encodePath: addLeadingSlash$1,
-    decodePath: addLeadingSlash$1
+    encodePath: addLeadingSlash,
+    decodePath: addLeadingSlash
   }
 };
 
@@ -713,22 +713,22 @@ function createHashHistory(props) {
     props = {};
   }
 
-  !canUseDOM ? invariant(false) : void 0;
+  !canUseDOM ?  invariant(false) : void 0;
   var globalHistory = window.history;
-  supportsGoWithoutReloadUsingHash();
+  var canGoWithoutReload = supportsGoWithoutReloadUsingHash();
   var _props = props,
       _props$getUserConfirm = _props.getUserConfirmation,
       getUserConfirmation = _props$getUserConfirm === void 0 ? getConfirmation : _props$getUserConfirm,
       _props$hashType = _props.hashType,
       hashType = _props$hashType === void 0 ? 'slash' : _props$hashType;
-  var basename = props.basename ? stripTrailingSlash(addLeadingSlash$1(props.basename)) : '';
+  var basename = props.basename ? stripTrailingSlash(addLeadingSlash(props.basename)) : '';
   var _HashPathCoders$hashT = HashPathCoders[hashType],
       encodePath = _HashPathCoders$hashT.encodePath,
       decodePath = _HashPathCoders$hashT.decodePath;
 
   function getDOMLocation() {
     var path = decodePath(getHashPath());
-    if (basename) path = stripBasename$1(path, basename);
+    if (basename) path = stripBasename(path, basename);
     return createLocation(path);
   }
 
@@ -1885,8 +1885,9 @@ function (_React$Component) {
  * The public API for a <Router> that stores location in memory.
  */
 
+var MemoryRouter =
 /*#__PURE__*/
-(function (_React$Component) {
+function (_React$Component) {
   _inheritsLoose(MemoryRouter, _React$Component);
 
   function MemoryRouter() {
@@ -1911,10 +1912,11 @@ function (_React$Component) {
   };
 
   return MemoryRouter;
-})(react.Component);
+}(react.Component);
 
+var Lifecycle =
 /*#__PURE__*/
-(function (_React$Component) {
+function (_React$Component) {
   _inheritsLoose(Lifecycle, _React$Component);
 
   function Lifecycle() {
@@ -1940,7 +1942,7 @@ function (_React$Component) {
   };
 
   return Lifecycle;
-})(react.Component);
+}(react.Component);
 
 var cache$1 = {};
 var cacheLimit$1 = 10000;
@@ -2041,7 +2043,7 @@ function (_React$Component) {
     var _this = this;
 
     return react.createElement(context.Consumer, null, function (context$1) {
-      !context$1 ? invariant(false) : void 0;
+      !context$1 ?  invariant(false) : void 0;
       var location = _this.props.location || context$1.location;
       var match = _this.props.computedMatch ? _this.props.computedMatch // <Switch> already computed the match for us
       : _this.props.path ? matchPath(location.pathname, _this.props) : context$1.match;
@@ -2063,27 +2065,27 @@ function (_React$Component) {
 
       return react.createElement(context.Provider, {
         value: props
-      }, props.match ? children ? typeof children === "function" ? children(props) : children : component ? react.createElement(component, props) : render ? render(props) : null : typeof children === "function" ? children(props) : null);
+      }, props.match ? children ? typeof children === "function" ?  children(props) : children : component ? react.createElement(component, props) : render ? render(props) : null : typeof children === "function" ?  children(props) : null);
     });
   };
 
   return Route;
 }(react.Component);
 
-function addLeadingSlash(path) {
+function addLeadingSlash$1(path) {
   return path.charAt(0) === "/" ? path : "/" + path;
 }
 
 function addBasename(basename, location) {
   if (!basename) return location;
   return _extends({}, location, {
-    pathname: addLeadingSlash(basename) + location.pathname
+    pathname: addLeadingSlash$1(basename) + location.pathname
   });
 }
 
-function stripBasename(basename, location) {
+function stripBasename$1(basename, location) {
   if (!basename) return location;
-  var base = addLeadingSlash(basename);
+  var base = addLeadingSlash$1(basename);
   if (location.pathname.indexOf(base) !== 0) return location;
   return _extends({}, location, {
     pathname: location.pathname.substr(base.length)
@@ -2096,7 +2098,7 @@ function createURL(location) {
 
 function staticHandler(methodName) {
   return function () {
-     invariant(false) ;
+      invariant(false) ;
   };
 }
 
@@ -2109,8 +2111,9 @@ function noop() {}
  */
 
 
+var StaticRouter =
 /*#__PURE__*/
-(function (_React$Component) {
+function (_React$Component) {
   _inheritsLoose(StaticRouter, _React$Component);
 
   function StaticRouter() {
@@ -2166,10 +2169,10 @@ function noop() {}
 
     var history = {
       createHref: function createHref(path) {
-        return addLeadingSlash(basename + createURL(path));
+        return addLeadingSlash$1(basename + createURL(path));
       },
       action: "POP",
-      location: stripBasename(basename, createLocation(location)),
+      location: stripBasename$1(basename, createLocation(location)),
       push: this.handlePush,
       replace: this.handleReplace,
       go: staticHandler(),
@@ -2185,7 +2188,7 @@ function noop() {}
   };
 
   return StaticRouter;
-})(react.Component);
+}(react.Component);
 
 /**
  * The public API for rendering the first <Route> that matches.
@@ -2206,7 +2209,7 @@ function (_React$Component) {
     var _this = this;
 
     return react.createElement(context.Consumer, null, function (context) {
-      !context ? invariant(false) : void 0;
+      !context ?  invariant(false) : void 0;
       var location = _this.props.location || context.location;
       var element, match; // We use React.Children.forEach instead of React.Children.toArray().find()
       // here because toArray adds keys to all child elements and we do not want
@@ -2232,7 +2235,7 @@ function (_React$Component) {
   return Switch;
 }(react.Component);
 
-react.useContext;
+var useContext = react.useContext;
 
 /**
  * The public API for a <Router> that uses HTML5 history.
@@ -2271,8 +2274,9 @@ function (_React$Component) {
  * The public API for a <Router> that uses window.location.hash.
  */
 
+var HashRouter =
 /*#__PURE__*/
-(function (_React$Component) {
+function (_React$Component) {
   _inheritsLoose(HashRouter, _React$Component);
 
   function HashRouter() {
@@ -2297,7 +2301,7 @@ function (_React$Component) {
   };
 
   return HashRouter;
-})(react.Component);
+}(react.Component);
 
 var resolveToLocation = function resolveToLocation(to, currentLocation) {
   return typeof to === "function" ? to(currentLocation) : to;
@@ -2373,7 +2377,7 @@ var Link = forwardRef(function (_ref2, forwardedRef) {
       rest = _objectWithoutPropertiesLoose(_ref2, ["component", "replace", "to", "innerRef"]);
 
   return react.createElement(context.Consumer, null, function (context) {
-    !context ? invariant(false) : void 0;
+    !context ?  invariant(false) : void 0;
     var history = context.history;
     var location = normalizeToLocation(resolveToLocation(to, context.location), context.location);
     var href = location ? history.createHref(location) : "";
@@ -2422,7 +2426,7 @@ function joinClassnames() {
  */
 
 
-forwardRef$1(function (_ref, forwardedRef) {
+var NavLink = forwardRef$1(function (_ref, forwardedRef) {
   var _ref$ariaCurrent = _ref["aria-current"],
       ariaCurrent = _ref$ariaCurrent === void 0 ? "page" : _ref$ariaCurrent,
       _ref$activeClassName = _ref.activeClassName,
@@ -2440,7 +2444,7 @@ forwardRef$1(function (_ref, forwardedRef) {
       rest = _objectWithoutPropertiesLoose(_ref, ["aria-current", "activeClassName", "activeStyle", "className", "exact", "isActive", "location", "sensitive", "strict", "style", "to", "innerRef"]);
 
   return react.createElement(context.Consumer, null, function (context) {
-    !context ? invariant(false) : void 0;
+    !context ?  invariant(false) : void 0;
     var currentLocation = locationProp || context.location;
     var toLocation = normalizeToLocation(resolveToLocation(to, currentLocation), currentLocation);
     var path = toLocation.pathname; // Regex taken from: https://github.com/pillarjs/path-to-regexp/blob/master/index.js#L202
